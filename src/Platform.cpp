d8f157158944695cf2234e3c28d5a27d63c0fc13
davixiao <avidxiao@gmail.com>
2021-07-23 20:50:59 -0700

    added a key map

--- src/Platform.cpp
+++ src/Platform.cpp
@@ -5,6 +5,25 @@
 #include <SDL2/SDL.h>
 #include "Platform.h"
 
+constexpr uint8_t keymap[16] = {
+  SDLK_x,
+  SDLK_1,
+  SDLK_2,
+  SDLK_3,
+  SDLK_q,
+  SDLK_w,
+  SDLK_e,
+  SDLK_a,
+  SDLK_s,
+  SDLK_d,
+  SDLK_z,
+  SDLK_c,
+  SDLK_4,
+  SDLK_r,
+  SDLK_f,
+  SDLK_v,
+};
+
 Platform::Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight) {
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -34,194 +53,33 @@ bool Platform::ProcessInput(uint8_t* keys) {
 	bool quit = false;
 
 	SDL_Event event;
-
-	while (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-			case SDL_QUIT:
-			{
+  while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+			case SDL_QUIT: {
 				quit = true;
-			} break;
-
-			case SDL_KEYDOWN:
-			{
-				switch (event.key.keysym.sym)
-				{
-					case SDLK_ESCAPE:
-					{
-						quit = true;
-					} break;
-
-					case SDLK_x:
-					{
-						keys[0] = 1;
-					} break;
-
-					case SDLK_1:
-					{
-						keys[1] = 1;
-					} break;
-
-					case SDLK_2:
-					{
-						keys[2] = 1;
-					} break;
-
-					case SDLK_3:
-					{
-						keys[3] = 1;
-					} break;
-
-					case SDLK_q:
-					{
-						keys[4] = 1;
-					} break;
-
-					case SDLK_w:
-					{
-						keys[5] = 1;
-					} break;
-
-					case SDLK_e:
-					{
-						keys[6] = 1;
-					} break;
-
-					case SDLK_a:
-					{
-						keys[7] = 1;
-					} break;
-
-					case SDLK_s:
-					{
-						keys[8] = 1;
-					} break;
-
-					case SDLK_d:
-					{
-						keys[9] = 1;
-					} break;
-
-					case SDLK_z:
-					{
-						keys[0xA] = 1;
-					} break;
-
-					case SDLK_c:
-					{
-						keys[0xB] = 1;
-					} break;
-
-					case SDLK_4:
-					{
-						keys[0xC] = 1;
-					} break;
-
-					case SDLK_r:
-					{
-						keys[0xD] = 1;
-					} break;
-
-					case SDLK_f:
-					{
-						keys[0xE] = 1;
-					} break;
-
-					case SDLK_v:
-					{
-						keys[0xF] = 1;
-					} break;
-				}
-			} break;
-
-			case SDL_KEYUP:
-			{
-				switch (event.key.keysym.sym)
-				{
-					case SDLK_x:
-					{
-						keys[0] = 0;
-					} break;
-
-					case SDLK_1:
-					{
-						keys[1] = 0;
-					} break;
-
-					case SDLK_2:
-					{
-						keys[2] = 0;
-					} break;
-
-					case SDLK_3:
-					{
-						keys[3] = 0;
-					} break;
-
-					case SDLK_q:
-					{
-						keys[4] = 0;
-					} break;
-
-					case SDLK_w:
-					{
-						keys[5] = 0;
-					} break;
-
-					case SDLK_e:
-					{
-						keys[6] = 0;
-					} break;
-
-					case SDLK_a:
-					{
-						keys[7] = 0;
-					} break;
-
-					case SDLK_s:
-					{
-						keys[8] = 0;
-					} break;
-
-					case SDLK_d:
-					{
-						keys[9] = 0;
-					} break;
-
-					case SDLK_z:
-					{
-						keys[0xA] = 0;
-					} break;
-
-					case SDLK_c:
-					{
-						keys[0xB] = 0;
-					} break;
-
-					case SDLK_4:
-					{
-						keys[0xC] = 0;
-					} break;
-
-					case SDLK_r:
-					{
-						keys[0xD] = 0;
-					} break;
-
-					case SDLK_f:
-					{
-						keys[0xE] = 0;
-					} break;
-
-					case SDLK_v:
-					{
-						keys[0xF] = 0;
-					} break;
-				}
-			} break;
+        break;
+			} 
+			case SDL_KEYDOWN: {
+        if (event.key.keysym.sym == SDLK_ESCAPE) 
+          quit = true;
+        else {
+          for (unsigned int i = 0; i < 16; ++i) {
+            if (event.key.keysym.sym == keymap[i]) {
+              keys[i] = 1;
+            }
+          }
+        }
+        break;
+      }
+			case SDL_KEYUP: {
+        for (unsigned int i = 0; i < 16; ++i) {
+          if (event.key.keysym.sym == keymap[i]) {
+            keys[i] = 0;
+          }
+        }
+        break;
+      }
 		}
 	}
-
 	return quit;
 }
